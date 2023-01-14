22b31903a3d0396729b0bd471511d94afcf8cdf1
clement koomson <ckoomson75@gmail.com>
2023-01-14 11:14:34 +0000

    Initial commit

---
+++ src/main/java/com/example/blog/enumeration/Role.java
@@ -0,0 +1,5 @@
+package com.example.blog.enumeration;
+
+public enum Role {
+    ADMIN,USER
+}
---
+++ src/main/java/com/example/blog/model/User.java
@@ -0,0 +1,60 @@
+package com.example.blog.model;
+
+import com.example.blog.enumeration.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String firstName;
+    private String lastname;
+    private String email;
+    private String password;
+    private String username;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private Boolean isEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}

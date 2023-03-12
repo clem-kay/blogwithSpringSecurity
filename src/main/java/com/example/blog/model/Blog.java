c9b3766b30a6df9461513dae8cc09e30d77e3824
clement koomson <ckoomson75@gmail.com>
2023-03-12 13:06:38 +0000

    refactored the code to suit blog post

--- src/main/java/com/example/blog/model/Blog.java
+++ src/main/java/com/example/blog/model/Blog.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "_blogPost")
+@Table(name = "_blogPosts")
 public class Blog {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
